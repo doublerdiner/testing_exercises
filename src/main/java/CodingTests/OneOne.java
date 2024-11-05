ad387e987b67035376f8ef07c2dcd76a800012cb
Chris Barclay <chris.barclay1@gmail.com>
2024-11-05 09:38:48 +0000

    Another solution

--- src/main/java/CodingTests/OneOne.java
+++ src/main/java/CodingTests/OneOne.java
@@ -3,19 +3,36 @@ package CodingTests;
 public class OneOne {
 
     public static void main(String[] args) {
-        System.out.println(hasUniqueCharacters("String"));
+
+        System.out.println(hasUniqueCharacters("Testthis"));
+
+        System.out.println(anotherSolution("Test"));
+    }
+
+    private static boolean anotherSolution(String testString) {
+        String stringToTest = testString.toLowerCase().replaceAll("//s", "").strip();
+        boolean[] boolArray = new boolean[128];
+        char[] charArray = stringToTest.toCharArray();
+        for(char character : charArray){
+            if(boolArray[character]){
+                return false;
+            }
+            boolArray[character] = true;
+        }
+        return true;
     }
 
     private static boolean hasUniqueCharacters(String testString) {
         char[] cleanString = testString.toLowerCase().replaceAll("\\s+", "").strip().toCharArray();
         StringBuilder answer = new StringBuilder();
-        for(int i=0; i<cleanString.length; i++){
-            char currentChar = cleanString[i];
-            if(answer.toString().contains(String.valueOf(currentChar))){
+        for(char character : cleanString){
+            if(answer.toString().contains(String.valueOf(character))){
                 return false;
             }
-            answer.append(currentChar);
+            answer.append(character);
         }
         return true;
     }
+
+
 }
