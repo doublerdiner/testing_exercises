ed16347104c2c3e7882bee8953c528f1fa5b7f07
Chris Barclay <chris.barclay1@gmail.com>
2025-10-10 20:49:20 +0100

    Coding Practice Question

---
+++ src/main/java/CodingPractice/MergeIntervals.java
@@ -0,0 +1,41 @@
+package CodingPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+        System.out.println(Arrays.deepToString(merge(intervals)));
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        if(intervals.length <= 1){
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+        int[] current = intervals[0];
+        merged.add(current);
+
+        for(int[] interval : intervals){
+            int currentEnd = current[1];
+            int nextStart = interval[0];
+            int nextEnd = interval[1];
+
+            if(currentEnd >= nextStart){
+                current[1] = Math.max(currentEnd, nextEnd);
+            }
+            else{
+                current = interval;
+                merged.add(interval);
+            }
+        }
+        return merged.toArray(new int[merged.size()][]);
+    }
+}
